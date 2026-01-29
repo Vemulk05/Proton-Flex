5603368d08d2f8d439d63aba27a3f0a3834851cf
Vemulk05 <arnavvemula1024@gmail.com>
2026-01-28 21:47:13 -0500

    Johnson counter step 2 Lab 1 362

--- src/main.c
+++ src/main.c
@@ -3,7 +3,7 @@
 
 //////////////////////////////////////////////////////////////////////////////
 
-const char* username = "username";
+const char* username = "vemula2";
 
 //////////////////////////////////////////////////////////////////////////////
 
@@ -11,10 +11,22 @@ void autotest();
 
 void init_outputs() {
     // fill in
+
+    //defining pins 22-25
+    io_bank0_hw -> io[22].ctrl = GPIO_FUNC_SIO << IO_BANK0_GPIO0_CTRL_FUNCSEL_LSB;
+    io_bank0_hw -> io[23].ctrl = GPIO_FUNC_SIO << IO_BANK0_GPIO0_CTRL_FUNCSEL_LSB;
+    io_bank0_hw -> io[24].ctrl = GPIO_FUNC_SIO << IO_BANK0_GPIO0_CTRL_FUNCSEL_LSB;
+    io_bank0_hw -> io[25].ctrl = GPIO_FUNC_SIO << IO_BANK0_GPIO0_CTRL_FUNCSEL_LSB;
+
+    //clearing leds to known state
+    sio_hw -> gpio_clr = (1u << 22) | (1u << 23) | (1u << 24) | (1u << 25);
+    //setting pins as output
+    sio_hw -> gpio_oe_set = (1u << 22) | (1u << 23) | (1u << 24) | (1u << 25);
 }
 
 void init_inputs() {
     // fill in
+    //gpio_init(21);
 }
 
 void init_keypad() {
@@ -28,20 +40,38 @@ int main() {
 
     // Leave commented until you actually need it.
     // Can take significantly longer to upload code when uncommented.
-    // autotest();
+    autotest();
     
     ////////////////////////////////////
     // All your code goes below.
     
+    init_outputs();
     
-    
-    // All your code goes above.
-    ////////////////////////////////////
+    //for(;;){
+        //setting each pin to HIGH
+        sio_hw -> gpio_set = 1u << 22;
+        sleep_ms(500);
+        sio_hw -> gpio_set = 1u << 23;
+        sleep_ms(500);
+        sio_hw -> gpio_set = 1u << 24;
+        sleep_ms(500);
+        sio_hw -> gpio_set = 1u << 25;
+        sleep_ms(500);
 
-    // An infinite loop is necessary to 
-    // ensure control flow remains with user.
-    for(;;);
+        //clearing each pin
+        sio_hw -> gpio_clr = 1u << 22;
+        sleep_ms(500);
+        sio_hw -> gpio_clr = 1u << 23;
+        sleep_ms(500);
+        sio_hw -> gpio_clr = 1u << 24;
+        sleep_ms(500);
+        sio_hw -> gpio_clr = 1u << 25;
+        sleep_ms(500);
+    //}
 
     // Never reached.
     return 0;
+
+    // All your code goes above.
+    ////////////////////////////////////
 }
